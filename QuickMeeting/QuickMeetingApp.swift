0eb0f1c5081be3607eeb33cb0f6e9db96f2175e7
Aleksandr Kitov <kitov.nn@gmail.com>
2026-05-04 21:14:13 +0300

    Initial Commit

---
+++ QuickMeeting/QuickMeetingApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuickMeetingApp.swift
+//  QuickMeeting
+//
+//  Created by heymdall on 04.05.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuickMeetingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
