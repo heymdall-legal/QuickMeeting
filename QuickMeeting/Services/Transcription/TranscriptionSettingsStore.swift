b8a5bd0188cee45f3a59adc849de137172c87608
Aleksandr Kitov <kitov.nn@gmail.com>
2026-08-17 23:00:41 +0300

    feat: persist automatic transcription setting

--- QuickMeeting/Services/Transcription/TranscriptionSettingsStore.swift
+++ QuickMeeting/Services/Transcription/TranscriptionSettingsStore.swift
@@ -21,6 +21,7 @@ nonisolated struct TranscriptionSettingsStore: TranscriptionLanguageStoring {
     private let languageCodeKey = "transcription.languageCode"
     private let ctcModeKey = "transcription.ctcMode"
     private let llmCorrectionEnabledKey = "transcription.llmCorrectionEnabled"
+    private let automaticTranscriptionEnabledKey = "transcription.automaticTranscriptionEnabled"
 
     init(userDefaults: UserDefaults = .standard) {
         self.userDefaults = userDefaults
@@ -44,7 +45,8 @@ nonisolated struct TranscriptionSettingsStore: TranscriptionLanguageStoring {
         return TranscriptionPipelineOptions(
             languageCode: languageCode(),
             ctcMode: mode,
-            isLLMCorrectionEnabled: userDefaults.bool(forKey: llmCorrectionEnabledKey)
+            isLLMCorrectionEnabled: userDefaults.bool(forKey: llmCorrectionEnabledKey),
+            isAutomaticTranscriptionEnabled: userDefaults.bool(forKey: automaticTranscriptionEnabledKey)
         )
     }
 
@@ -52,6 +54,7 @@ nonisolated struct TranscriptionSettingsStore: TranscriptionLanguageStoring {
         saveLanguageCode(options.languageCode)
         userDefaults.set(options.ctcMode.rawValue, forKey: ctcModeKey)
         userDefaults.set(options.isLLMCorrectionEnabled, forKey: llmCorrectionEnabledKey)
+        userDefaults.set(options.isAutomaticTranscriptionEnabled, forKey: automaticTranscriptionEnabledKey)
     }
 }
 
--- QuickMeetingTests/TranscriptionSettingsStoreTests.swift
+++ QuickMeetingTests/TranscriptionSettingsStoreTests.swift
@@ -11,8 +11,24 @@ struct TranscriptionSettingsStoreTests {
         #expect(store.pipelineOptions() == TranscriptionPipelineOptions(
             languageCode: nil,
             ctcMode: .off,
-            isLLMCorrectionEnabled: false
+            isLLMCorrectionEnabled: false,
+            isAutomaticTranscriptionEnabled: false
         ))
+    }
+
+    @Test
+    func automaticTranscriptionDefaultsToDisabled() {
+        let store = TranscriptionSettingsStore(userDefaults: makeDefaults())
+
+        #expect(store.pipelineOptions().isAutomaticTranscriptionEnabled == false)
+    }
+
+    @Test
+    func automaticTranscriptionRoundTripsWithPipelineOptions() {
+        let store = TranscriptionSettingsStore(userDefaults: makeDefaults())
+        store.savePipelineOptions(.init(isAutomaticTranscriptionEnabled: true))
+
+        #expect(store.pipelineOptions().isAutomaticTranscriptionEnabled == true)
     }
 
     @Test
@@ -24,14 +40,16 @@ struct TranscriptionSettingsStoreTests {
         store.savePipelineOptions(.init(
             languageCode: "fr",
             ctcMode: .ctc06b,
-            isLLMCorrectionEnabled: true
+            isLLMCorrectionEnabled: true,
+            isAutomaticTranscriptionEnabled: true
         ))
 
         #expect(store.languageCode() == "fr")
         #expect(store.pipelineOptions() == TranscriptionPipelineOptions(
             languageCode: "fr",
             ctcMode: .ctc06b,
-            isLLMCorrectionEnabled: true
+            isLLMCorrectionEnabled: true,
+            isAutomaticTranscriptionEnabled: true
         ))
     }
 
